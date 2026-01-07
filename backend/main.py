a884aa723063fafba8d97332655d90fc38048c01
user.email <vidyou874@gmai.com>
2026-01-07 18:50:09 +0530

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 
 from fastapi import FastAPI
 from models import MessageRequest
-from database import save_to_db
+# from database import save_to_db
+from database import save_to_db, get_all_messages
 
 app = FastAPI()
 
@@ -14,3 +15,16 @@ def store_message(msg: MessageRequest):
         "status": "ok",
         "message": msg.text
     }
+
+
+
+@app.get("/messages")
+def list_messages():
+
+    data = get_all_messages()
+
+    return {
+        "count": len(data),
+        "items": data
+    }
+
